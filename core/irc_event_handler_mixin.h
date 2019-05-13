64637d92cd6adc431e852ad9014c3a9100a630b8
duke <duke@localhost.localdomain>
2019-05-12 22:24:22 -0500

    Add comments

--- core/irc_event_handler_mixin.h
+++ core/irc_event_handler_mixin.h
@@ -9,10 +9,12 @@ namespace core {
   template <class EventType>
   class IrcEventHandlerMixin {
   public:
+    // Add events to the head of the list, because events triggering destruction are the first events to get added
     void add_event_handler(std::shared_ptr<EventType> event_handler) {
       m_event_handlers.insert(m_event_handlers.begin(), event_handler);
     }
   protected:
+    // Trigger events from last added to first added
     void send_event(std::function<void(EventType &)> handler) {
       for (auto it = m_event_handlers.begin(); it != m_event_handlers.end(); it++) {
         handler(**it);
